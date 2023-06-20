4349b2fbcbce1d6fc751b2dd611230a77edf837f
urnamu <36816378+urnamu@users.noreply.github.com>
2023-06-20 11:29:43 +0300

    Update home_task.sql

--- home_task.sql
+++ home_task.sql
@@ -42,6 +42,6 @@ SELECT film_id, title FROM film WHERE NOT EXISTS (SELECT film_id FROM inventory 
 Вивести топ 3 актори, які найбільше зʼявлялись в категорії фільмів “Children”.
 */
 
-SELECT COUNT(film.film_id), actor.actor_id, actor.first_name, actor.last_name FROM film, film_actor, actor, film_category WHERE film_category.film_id=film.film_id AND film_category.category_id=3 AND actor.actor_id=film_actor.actor_id AND film_actor.film_id=film.film_id GROUP BY actor.actor_id ORDER by COUNT(film.film_id) DESC LIMIT 7;
+SELECT COUNT(film.film_id), actor.actor_id, actor.first_name, actor.last_name FROM film, film_actor, actor, film_category WHERE film_category.film_id=film.film_id AND film_category.category_id=3 AND actor.actor_id=film_actor.actor_id AND film_actor.film_id=film.film_id GROUP BY actor.actor_id ORDER by COUNT(film.film_id) DESC LIMIT 5;
 
 /* не зовсім коректна постановка завдання, тому що 4 актори мають однаковий рейтинг за цією вибіркою */
